53f7871fbb047a112a0ffd4590b163ecf88f6094
Francisco <francisco_nascimento@outlook.com.br>
2016-02-17 18:00:03 -0300

    Modificado construtor de copia

--- main.cpp
+++ main.cpp
@@ -9,8 +9,8 @@ int main(int argc, char **argv)
 {
     Plants planta1;
     
-    cout << "Plants atacou " << planta1.atacarZumbis();
-    cout << "Plants recebeu um ataque " << planta1.receberAtaque()
+    planta1.atacarZumbis();
+    planta1.receberAtaque();
     
 	printf("hello world\n");
 	return 0;
